b87fe0c0a83617b453deaf4f7b4915439b4126cd
Charles Joshua Houser <cjohouser@gmail.com>
2020-11-16 15:24:44 -0800

    post representation

--- posts/models.py
+++ posts/models.py
@@ -4,3 +4,6 @@ class Post(models.Model):
     title = models.CharField(max_length=256)
     creation_date = models.DateTimeField()
     content = models.TextField()
+
+    def __str__(self):
+        return self.title
